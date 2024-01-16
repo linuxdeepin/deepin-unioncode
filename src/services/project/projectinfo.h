b485d13e96097b22387fce43dde283e463a8b71c
Lu Zhen <luzhen@uniontech.com>
2024-01-16 15:01:48 +0800

    refactor: remove unused code
    Log:
    Change-Id: Ib3f02e308ac0bcbb6d51fc0c5c765ffceb2a0b89

--- src/services/project/projectinfo.h
+++ src/services/project/projectinfo.h
@@ -24,46 +24,42 @@ public:
     ProjectInfo() : data({}) {}
     ProjectInfo(const ProjectInfo &other) {data = other.data;}
 
+    inline void setLanguage(const QString &language) {data["Language"] = language;}
+    inline QString language() const {return data["Language"].toString();}
+    inline void setKitName(const QString &kitName) {data["KitName"] = kitName;}
+    inline QString kitName() const {return data["KitName"].toString();}
+    inline void setBuildFolder(const QString &buildPath) {data["BuildFolder"] = buildPath;}
+    inline QString buildFolder() const {return data["BuildFolder"].toString();}
+    inline void setWorkspaceFolder(const QString &workspaceFolder) {data["WorkspaceFolder"] = workspaceFolder;}
+    inline QString workspaceFolder() const {return data["WorkspaceFolder"].toString();}
+    inline void setBuildType(const QString &buildType) {data["BuildType"] = buildType;}
+    inline QString buildType() const {return data["BuildType"].toString();}
+    inline void setBuildProgram(const QString &program) {data["BuildProgram"] = program;}
+    inline QString buildProgram() const {return data["BuildProgram"].toString();}
+    inline void setConfigCustomArgs(const QStringList &args) {data["ConfigCustomArgs"] = args;}
+    inline QStringList configCustomArgs() const {return data["ConfigCustomArgs"].toStringList();}
+    inline void setSourceFiles(const QSet<QString> &files) {data["SourceFiles"] = QVariant::fromValue(files);}
+    inline QSet<QString> sourceFiles() const {return qvariant_cast<QSet<QString>>(data["SourceFiles"]);}
+    inline void setDetailInformation(bool isDetail) {data["DetailInformation"] = isDetail;}
+    inline bool detailInformation() const {return data["DetailInformation"].toBool();}
+    inline void setBuildCustomArgs(const QStringList &args) {data["BuildCustomArgs"] = args;}
+    inline QStringList buildCustomArgs() const {return data["BuildCustomArgs"].toStringList();}
+    inline void setCleanCustomArgs(const QStringList &args) {data["CleanCustomArgs"] = args;}
+    inline QStringList cleanCustomArgs() const {return data["CleanCustomArgs"].toStringList();}
+    inline void setRunProgram(const QString &program) {data["RunProgram"] = program;}
+    inline QString runProgram() const {return data["RunProgram"].toString();}
+    inline void setRunCustomArgs(const QStringList &args) {data["RunCustomArgs"] = args;}
+    inline QStringList runCustomArgs() const {return data["RunCustomArgs"].toStringList();}
+    inline void setRunWorkspaceDir(const QString &workspaceDir) {data["RunWorkspaceDir"] = workspaceDir;}
+    inline QString runWorkspaceDir() const {return data["RunWorkspaceDir"].toString();}
+    inline void setRunEnvironment(const QStringList &envs) {data["RunEnvironment"] = envs;}
+    inline QStringList runEnvironment() const {return data["RunEnvironment"].toStringList();}
+
     bool operator == (const ProjectInfo &other) const {return data == other.data;}
     bool isEmpty() const {return data.isEmpty();}
     inline bool hasKey(const QString &key) {return data.keys().contains(key);}
-
     inline void setProperty(const QString &key, QVariant value) {data[key] = value;}
     inline QVariant property(const QString &key) const {return data[key];}
-
-    inline void setLanguage(const QString &language) {data["Language"] = language;}
-    inline void setKitName(const QString &kitName) {data["KitName"] = kitName;}
-    inline void setBuildFolder(const QString &buildPath) {data["BuildFolder"] = buildPath;}
-    inline void setWorkspaceFolder(const QString &workspaceFolder) {data["WorkspaceFolder"] = workspaceFolder;}
-    inline void setBuildType(const QString &buildType) {data["BuildType"] = buildType;}
-    inline void setBuildProgram(const QString &program) {data["BuildProgram"] = program;}
-    inline void setConfigCustomArgs(const QStringList &args) {data["ConfigCustomArgs"] = args;}
-    inline void setSourceFiles(const QSet<QString> &files) {data["SourceFiles"] = QVariant::fromValue(files);}
-    inline void setDetailInformation(bool isDetail) {data["DetailInformation"] = isDetail;}
-
-    inline void setBuildCustomArgs(const QStringList &args) {data["BuildCustomArgs"] = args;}
-    inline void setCleanCustomArgs(const QStringList &args) {data["CleanCustomArgs"] = args;}
-    inline void setRunProgram(const QString &program) {data["RunProgram"] = program;}
-    inline void setRunCustomArgs(const QStringList &args) {data["RunCustomArgs"] = args;}
-    inline void setRunWorkspaceDir(const QString &workspaceDir) {data["RunWorkspaceDir"] = workspaceDir;}
-    inline void setRunEnvironment(const QStringList &envs) {data["RunEnvironment"] = envs;}
-
-    inline QString language() const {return data["Language"].toString();}
-    inline QString kitName() const {return data["KitName"].toString();}
-    inline QString buildFolder() const {return data["BuildFolder"].toString();}
-    inline QString workspaceFolder() const {return data["WorkspaceFolder"].toString();}
-    inline QString buildType() const {return data["BuildType"].toString();}
-    inline QString buildProgram() const {return data["BuildProgram"].toString();}
-    inline QStringList configCustomArgs() const {return data["ConfigCustomArgs"].toStringList();}
-    inline QSet<QString> sourceFiles() const {return qvariant_cast<QSet<QString>>(data["SourceFiles"]);}
-    inline bool detailInformation() const {return data["DetailInformation"].toBool();}
-
-    inline QStringList buildCustomArgs() const {return data["BuildCustomArgs"].toStringList();}
-    inline QStringList cleanCustomArgs() const {return data["CleanCustomArgs"].toStringList();}
-    inline QString runProgram() const {return data["RunProgram"].toString();}
-    inline QStringList runCustomArgs() const {return data["RunCustomArgs"].toStringList();}
-    inline QString runWorkspaceDir() const {return data["RunWorkspaceDir"].toString();}
-    inline QStringList runEnvironment() const {return data["RunEnvironment"].toStringList();}
 
     inline static void set(QStandardItem *root, const ProjectInfo &info)
     {
