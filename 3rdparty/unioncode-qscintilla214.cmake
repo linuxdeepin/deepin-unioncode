c0fe71586c6ed3d472772de93fcb4e51e12753f3
liuzhangjian <liuzhangjian@uniontech.com>
2024-01-18 16:01:47 +0800

    refactor: Replace `Scintilla` with `QScintilla`
    1.Remove the `Scintilla` source code
    2.Add the `QScintilla` source code

---
+++ 3rdparty/unioncode-qscintilla214.cmake
@@ -0,0 +1,7 @@
+include_directories(
+    ${CMAKE_SOURCE_DIR}/3rdparty/unioncode-qscintilla214/src
+)
+
+set(CMAKE_CXX_STANDARD 17)
+
+link_libraries(unioncode-qscintilla)
--- 3rdparty/unioncode-scintilla515/lexilla/test/examples/rust/Issue33.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let a: i128 = 42i128;
-    let b: u128 = 1337u128;
-    println!("{} + {} = {}", a, b, (a as u128) + b);
-}
