157398f152c334b4ca1216ccdb8912dde207c1a3
Lu Zhen <luzhen@uniontech.com>
2024-01-30 17:18:46 +0800

    fix: [python] cannot run in some condition
    Log:
    Change-Id: I9a6f898adb4283a09c01c6739490a212475dbd6e

--- src/framework/backtrace/backtrace.cpp
+++ src/framework/backtrace/backtrace.cpp
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <dfm-framework/backtrace/backtrace.h>
+#include <backtrace/backtrace.h>
 
 #include <QCoreApplication>
 #include <qloggingcategory.h>
