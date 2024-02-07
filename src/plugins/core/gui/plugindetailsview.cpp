bf17a4fae5f61047b0e5f999928d132fb5b9a1de
Lu Zhen <luzhen@uniontech.com>
2024-02-07 13:54:25 +0800

    feat: [extension] could display plugin readme
    Log:
    Change-Id: I6540edc77716ed299a7e2b3afb90dfd2f0a69aa0

---
+++ src/plugins/core/gui/plugindetailsview.cpp
@@ -0,0 +1,81 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+#include "plugindetailsview.h"
+
+#include <DWidget>
+#include <DLabel>
+#include <DTextEdit>
+#include <DListWidget>
+
+#include <QGridLayout>
+#include <QDesktopServices>
+#include <QWebEngineView>
+
+DWIDGET_USE_NAMESPACE
+
+DetailsView::DetailsView(QWidget *parent)
+    : DWidget(parent)
+{
+    setupUi();
+}
+
+void DetailsView::update(const dpf::PluginMetaObjectPointer &metaInfo)
+{
+    if (metaInfo.isNull())
+        return;
+
+    MetaInfo updateMetaInfo;
+    for (auto depend : metaInfo->depends()) {
+        updateMetaInfo.dependency += depend.toString();
+        if (depend.name() != metaInfo->depends().back().name()) {
+            updateMetaInfo.dependency += "、";
+        }
+    }
+    for (QString &text : metaInfo->license()) {
+        updateMetaInfo.license += text.simplified();
+        if (text != metaInfo->license().back()) {
+            updateMetaInfo.license += "、";
+        }
+    }
+    updateMetaInfo.name = metaInfo->name();
+    updateMetaInfo.version = metaInfo->version();
+    updateMetaInfo.compatibleVersion = metaInfo->compatVersion();
+    updateMetaInfo.vendor = metaInfo->vendor();
+    updateMetaInfo.copyright = metaInfo->copyright();
+    updateMetaInfo.category = metaInfo->category();
+    updateMetaInfo.url = metaInfo->urlLink();
+    updateMetaInfo.description = metaInfo->description();
+
+    metaInfoLabel->setText(updateMetaInfo.toHtml());
+}
+
+void DetailsView::setupUi()
+{
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setSpacing(0);
+    mainLayout->setMargin(0);
+    auto detailLayout = new QHBoxLayout(this);
+
+    metaInfoLabel = new DLabel(this);
+    metaInfoLabel->setText(MetaInfo().toHtml());
+    metaInfoLabel->setOpenExternalLinks(true);
+    metaInfoLabel->setWordWrap(true);
+
+    QLabel *iconLabel = new QLabel(this);
+    iconLabel->setPixmap(QIcon::fromTheme("plugins-navigation").pixmap(QSize(128, 128)));
+
+    auto webViewLayout = new QHBoxLayout(this);
+    QWebEngineView *webView = new QWebEngineView(this);
+    // TODO(mozart): load from local.
+//    webView->load(QUrl::fromLocalFile("url"));
+    webView->load(QUrl("https://ecology.chinauos.com/adaptidentification/doc_new/#document2?dirid=656d40a9bd766615b0b02e5e"));
+    webView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    webViewLayout->addWidget(webView);
+
+    detailLayout->addWidget(iconLabel);
+    detailLayout->addWidget(metaInfoLabel, 1);
+    detailLayout->setContentsMargins(64, 0, 0, 0);
+    mainLayout->addLayout(detailLayout);
+    mainLayout->addLayout(webViewLayout);
+}
--- src/plugins/core/gui/pluginsui.h
+++ src/plugins/core/gui/pluginsui.h
@@ -8,16 +8,10 @@
 #include "framework/lifecycle/pluginmetaobject.h"
 
 #include <DAbstractDialog>
-#include <DLabel>
 #include <DPushButton>
 #include <DDialogButtonBox>
-#include <DTextEdit>
-#include <DListWidget>
 
-#include <QGridLayout>
 #include <QSpacerItem>
-
-DWIDGET_USE_NAMESPACE
 
 class DetailsView;
 class QDialogButtonBox;
@@ -46,43 +40,6 @@ private:
     DetailsView *pluginDetailView = nullptr;
 };
 
-class DetailsView : public DTK_WIDGET_NAMESPACE::DWidget
-{
-    Q_OBJECT
-public:
-    DetailsView(QWidget *parent = nullptr);
 
-    void update(dpf::PluginMetaObjectPointer plugin);
-
-private:
-    QGridLayout *gridLayout = nullptr;
-    QVBoxLayout *vboxLayout_1 = nullptr;
-    QVBoxLayout *vboxLayout_2 = nullptr;
-    QVBoxLayout *vboxLayout_3 = nullptr;
-    QSpacerItem *spacerItem_1 = nullptr;
-    QSpacerItem *spacerItem_2 = nullptr;
-    QSpacerItem *spacerItem_3 = nullptr;
-    DLabel *name = nullptr;
-    DLabel *version = nullptr;
-    DLabel *compatVersion = nullptr;
-    DLabel *vendor = nullptr;
-    DLabel *copyright = nullptr;
-    DLabel *category = nullptr;
-    DLabel *urlLink = nullptr;
-    DTextEdit *description = nullptr;
-    DTextEdit *license = nullptr;
-    DListWidget *dependencies = nullptr;
-
-    DLabel *nameLabel = nullptr;
-    DLabel *versionLabel = nullptr;
-    DLabel *compatVersionLabel = nullptr;
-    DLabel *vendorLabel = nullptr;
-    DLabel *copyrightLabel = nullptr;
-    DLabel *categoryLabel = nullptr;
-    DLabel *urlLabel = nullptr;
-    DLabel *licenseLabel = nullptr;
-    DLabel *descriptionLabel = nullptr;
-    DLabel *dependenciesLabel = nullptr;
-};
 
 #endif // PLUGINDIALOG_H
