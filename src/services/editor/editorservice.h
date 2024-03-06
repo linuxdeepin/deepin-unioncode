83b15585ea5ff455aab24e7bfb1c47c40547b0b1
liuzhangjian <liuzhangjian@uniontech.com>
2024-03-06 11:29:56 +0800

    refactor: [editor] Add interfaces
    1.Add the interface to the modified file list
    2.Added an interface for saving all files
    
    Log: Add interfaces

--- src/services/editor/editorservice.h
+++ src/services/editor/editorservice.h
@@ -12,16 +12,15 @@
 namespace dpfservice {
 
 class EditorService final : public dpf::PluginService,
-        dpf::AutoServiceRegister<EditorService>
+                            dpf::AutoServiceRegister<EditorService>
 {
     Q_OBJECT
     Q_DISABLE_COPY(EditorService)
 
 public:
     explicit EditorService(QObject *parent = nullptr)
-        : dpf::PluginService (parent)
+        : dpf::PluginService(parent)
     {
-
     }
 
     static QString name()
@@ -32,6 +31,8 @@ public:
     DPF_INTERFACE(QString, getSelectedText);
     DPF_INTERFACE(QString, getCursorBeforeText);
     DPF_INTERFACE(QString, getCursorBehindText);
+    DPF_INTERFACE(QStringList, modifiedFiles);
+    DPF_INTERFACE(void, saveAll);
     DPF_INTERFACE(void, replaceSelectedText, const QString &);
     DPF_INTERFACE(void, insertText, const QString &);
     DPF_INTERFACE(void, showTips, const QString &tips);
@@ -40,5 +41,5 @@ public:
     DPF_INTERFACE(void, registSciLexerProxy, const QString &language, AbstractLexerProxy *proxy);
 };
 
-} // namespace dpfservice
-#endif // EDITORSERVICE_H
+}   // namespace dpfservice
+#endif   // EDITORSERVICE_H
