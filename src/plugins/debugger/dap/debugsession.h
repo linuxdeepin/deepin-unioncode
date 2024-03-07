7421077c3cfbb6fd482c023a5cdf4f23dee87fa4
ZHANGTINGAN <zhangtingan@uniontech.com>
2024-03-07 13:39:06 +0800

    feat: [debugger] view of watch-data area
    view of watch data area

--- src/plugins/debugger/dap/debugsession.h
+++ src/plugins/debugger/dap/debugsession.h
@@ -99,6 +99,7 @@ public:
     void setName(dap::string &name) override;
 
     bool getLocals(dap::integer frameId, IVariables *out) override;
+    bool getVariables(dap::integer variablesRef, IVariables *out, dap::integer depth = 0);
 
     dap::Session *getDapSession() const;
     dap::RawDebugSession *getRawSession() const;
@@ -127,7 +128,6 @@ private:
     dap::Source getRawSource(QUrl &uri);
     void cancelAllRequests();
 
-    bool getVariables(dap::integer variablesRef, IVariables *out, dap::integer depth = 0);
 
     QSharedPointer<dap::RawDebugSession> raw;
     QSharedPointer<RunTimeCfgProvider> rtCfgProvider;
