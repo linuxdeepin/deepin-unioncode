abb9e55cfdb72781ad5533ab83e1195a740afec1
Lu Zhen <luzhen@uniontech.com>
2024-03-07 16:44:22 +0800

    fix: cannot find lthread
    Log:
    Change-Id: Iebc4c7711c2f42f57964b093a5a47ea95846ebe6

--- 3rdparty/unioncode-jsonrpccpp/libjson-rpc-cpp/cmake/FindThreads.cmake
+++ 3rdparty/unioncode-jsonrpccpp/libjson-rpc-cpp/cmake/FindThreads.cmake
@@ -60,7 +60,8 @@ else()
         CHECK_LIBRARY_EXISTS(pthreads pthread_create "" CMAKE_HAVE_PTHREADS_CREATE)
         if(CMAKE_HAVE_PTHREADS_CREATE)
             #          set(CMAKE_THREAD_LIBS_INIT "-lpthreads") // mozart comment.
-            if (NOT ${CMAKE_SYSTEM_PROCESSOR} STREQUAL "mips64")
+            if (NOT ${CMAKE_SYSTEM_PROCESSOR} STREQUAL "mips64"
+                    AND NOT ${CMAKE_SYSTEM_PROCESSOR} STREQUAL "sw_64")
                 set(CMAKE_THREAD_LIBS_INIT "-lpthreads")
             endif()
             set(CMAKE_HAVE_THREADS_LIBRARY 1)
