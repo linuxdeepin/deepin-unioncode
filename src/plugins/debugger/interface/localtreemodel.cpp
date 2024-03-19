8560be3c3a47a6ec5c31bee117e758330d0907b4
ZHANGTINGAN <zhangtingan@uniontech.com>
2024-03-19 10:28:01 +0800

    fix: [debugger] gdb frozen when get variables
    prettyprinters scripts makes gdb frozen

--- src/plugins/debugger/interface/localtreemodel.cpp
+++ src/plugins/debugger/interface/localtreemodel.cpp
@@ -175,6 +175,8 @@ void LocalTreeModel::clear()
 {
     QWriteLocker locker(&mutex);
     items.clear();
+    locker.unlock();
+
     if (rootItem) {
         beginRemoveRows(rootItem->index(), 0, rootItem->childCount() - 1);
         rootItem->removeChildren();
