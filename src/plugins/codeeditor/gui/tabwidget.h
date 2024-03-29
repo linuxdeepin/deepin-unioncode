9fc495a05492b6dc93e19bac21c9857add27dfcd
liuzhangjian <liuzhangjian@uniontech.com>
2024-03-29 13:47:52 +0800

    refactor: [editor] Refactor the AI prompt completion information display mode
    as title
    
    Log: refactor

--- src/plugins/codeeditor/gui/tabwidget.h
+++ src/plugins/codeeditor/gui/tabwidget.h
@@ -36,6 +36,7 @@ public:
     Q_INVOKABLE void showTips(const QString &tips);
     Q_INVOKABLE void insertText(const QString &text);
     Q_INVOKABLE void undo();
+    Q_INVOKABLE void setCompletion(const QString &info, const QIcon &icon, const QKeySequence &key);
     void gotoNextPosition();
     void gotoPreviousPosition();
     bool checkAndResetSaveState(const QString &fileName);
