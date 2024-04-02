dea427c7344c9e6b03bd571cbad3f201b3391706
Lu Zhen <luzhen@uniontech.com>
2024-04-02 10:36:31 +0800

    fix: [cxx] ninja build failed
    Log:
    Change-Id: I875abebc6b8aac5b9645bc7e4bacddb5b2ae0ba1

--- src/base/abstractoutputparser.h
+++ src/base/abstractoutputparser.h
@@ -2,8 +2,8 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#ifndef IOUTPUTPARSER_H
-#define IOUTPUTPARSER_H
+#ifndef ABSTRACTOUTPUTPARSER_H
+#define ABSTRACTOUTPUTPARSER_H
 
 #include "common/type/task.h"
 #include "common/widget/outputpane.h"
@@ -49,4 +49,4 @@ private:
     AbstractOutputParser *outParser = nullptr;
 };
 
-#endif // IOUTPUTPARSER_H
+#endif // ABSTRACTOUTPUTPARSER_H
--- src/plugins/cxx/ninja/project/ninjaasynparse.cpp
+++ src/plugins/cxx/ninja/project/ninjaasynparse.cpp
@@ -92,14 +92,16 @@ void NinjaAsynParse::createRows(const QString &path)
             QString childPath = dirItera.next().remove(0, rootPath.size());
             QFileSystemWatcher::addPath(dirItera.filePath());
             QStandardItem *item = findItem(childPath);
-            QIcon icon = CustomIcons::icon(dirItera.fileInfo());
-            auto newItem = new QStandardItem(icon, dirItera.fileName());
-            newItem->setToolTip(dirItera.filePath());
-            if (!item) {
-                d->rows.append(newItem);
-            } else {
-                item->appendRow(newItem);
-            }
+            QMetaObject::invokeMethod(this, [&](){
+                QIcon icon = CustomIcons::icon(dirItera.fileInfo());
+                auto newItem = new QStandardItem(icon, dirItera.fileName());
+                newItem->setToolTip(dirItera.filePath());
+                if (!item) {
+                    d->rows.append(newItem);
+                } else {
+                    item->appendRow(newItem);
+                }
+            });
         }
     }
     {// 避免变量冲突 迭代文件
@@ -111,14 +113,17 @@ void NinjaAsynParse::createRows(const QString &path)
         while (fileItera.hasNext()) {
             QString childPath = fileItera.next().remove(0, rootPath.size());
             QStandardItem *item = findItem(childPath);
-            QIcon icon = CustomIcons::icon(fileItera.fileInfo());
-            auto newItem = new QStandardItem(icon, fileItera.fileName());
-            newItem->setToolTip(fileItera.filePath());
-            if (!item) {
-                d->rows.append(newItem);
-            } else {
-                item->appendRow(newItem);
-            }
+            // run in main thread.
+            QMetaObject::invokeMethod(this, [&](){
+                QIcon icon = CustomIcons::icon(fileItera.fileInfo());
+                auto newItem = new QStandardItem(icon, fileItera.fileName());
+                newItem->setToolTip(fileItera.filePath());
+                if (!item) {
+                    d->rows.append(newItem);
+                } else {
+                    item->appendRow(newItem);
+                }
+            });
         }
     }
 }
