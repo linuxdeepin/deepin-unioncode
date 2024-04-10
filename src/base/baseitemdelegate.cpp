7de38ec849de7ca9c2bf8a204fe106e67639994c
liuzhangjian <liuzhangjian@uniontech.com>
2024-04-10 09:31:42 +0800

    feat: Provide a `BaseItemDelegate` for the view
    as title
    
    Log: BaseItemDelegate

--- src/base/baseitemdelegate.cpp
+++ src/base/baseitemdelegate.cpp
@@ -2,25 +2,26 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include "filebrowserdelegate.h"
+#include "baseitemdelegate.h"
 
 #include <DGuiApplicationHelper>
 
 DGUI_USE_NAMESPACE
 
-FileBrowserDelegate::FileBrowserDelegate(QObject *parent)
+BaseItemDelegate::BaseItemDelegate(QObject *parent)
     : QStyledItemDelegate(parent)
 {
 }
 
-void FileBrowserDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+void BaseItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
     QStyleOptionViewItem iOption = option;
+
     if (!option.state.testFlag(QStyle::State_HasFocus)) {
         QColor color;
         DGuiApplicationHelper::instance()->themeType() == DGuiApplicationHelper::LightType
-                ? color = Qt::black
-                : color = QColor("#c5c8c9");
+            ? color = Qt::black
+            : color = QColor("#c5c8c9");
         iOption.palette.setColor(QPalette::Text, color);
     }
 
