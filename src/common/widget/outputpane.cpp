7715f8fe8f60c6d24b7042ff19d8a8ad57549ff6
liuzhangjian <liuzhangjian@uniontech.com>
2024-04-12 16:22:05 +0800

    feat: [find] Added find support for output logs
    as title
    
    Log: Find for output

--- src/common/widget/outputpane.cpp
+++ src/common/widget/outputpane.cpp
@@ -5,9 +5,12 @@
 #include "outputpane.h"
 #include "common/common.h"
 
+#include <DPlainTextEdit>
 #include <DScrollBar>
 #include <DMenu>
+
 #include <QDebug>
+#include <QVBoxLayout>
 
 /**
  * @brief Output text color.
@@ -17,11 +20,12 @@ const QColor kErrorMessageTextColor(255, 108, 108);
 const QColor kMessageOutput(0, 135, 135);
 constexpr int kDefaultMaxCharCount = 10000000;
 
+DWIDGET_USE_NAMESPACE
+
 class OutputPanePrivate
 {
 public:
-    explicit OutputPanePrivate(QTextDocument *document)
-        : cursor(document)
+    explicit OutputPanePrivate()
     {
     }
 
@@ -33,15 +37,14 @@ public:
     bool scrollToBottom = true;
     int maxCharCount = kDefaultMaxCharCount;
     QTextCursor cursor;
+    DPlainTextEdit *outputEdit = nullptr;
     DMenu *menu = nullptr;
 };
 
 OutputPane::OutputPane(QWidget *parent)
-    : DPlainTextEdit(parent)
-    , d(new OutputPanePrivate(document()))
+    : QWidget(parent),
+      d(new OutputPanePrivate())
 {
-    setReadOnly(true);
-
     initUI();
 }
 
@@ -55,15 +58,24 @@ OutputPane::~OutputPane()
 
 void OutputPane::initUI()
 {
-    setLineWidth(0);
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+
+    d->outputEdit = new DPlainTextEdit(this);
+    d->outputEdit->setReadOnly(true);
+    d->outputEdit->setLineWidth(0);
+
+    d->cursor = QTextCursor(d->outputEdit->document());
 
     d->menu = new DMenu(this);
     d->menu->addActions(actionFactory());
+
+    mainLayout->addWidget(d->outputEdit);
 }
 
 void OutputPane::clearContents()
 {
-    clear();
+    d->outputEdit->clear();
 }
 
 QString OutputPane::normalizeNewlines(const QString &text)
@@ -75,7 +87,7 @@ QString OutputPane::normalizeNewlines(const QString &text)
 
 bool OutputPane::isScrollbarAtBottom() const
 {
-    return verticalScrollBar()->value() == verticalScrollBar()->maximum();
+    return d->outputEdit->verticalScrollBar()->value() == d->outputEdit->verticalScrollBar()->maximum();
 }
 
 QString OutputPane::doNewlineEnforcement(const QString &out)
@@ -88,7 +100,7 @@ QString OutputPane::doNewlineEnforcement(const QString &out)
     }
 
     if (s.endsWith(QLatin1Char('\n'))) {
-        d->enforceNewline = true; // make appendOutputInline put in a newline next time
+        d->enforceNewline = true;   // make appendOutputInline put in a newline next time
         s.chop(1);
     }
 
@@ -97,16 +109,16 @@ QString OutputPane::doNewlineEnforcement(const QString &out)
 
 void OutputPane::scrollToBottom()
 {
-    verticalScrollBar()->setValue(verticalScrollBar()->maximum());
+    d->outputEdit->verticalScrollBar()->setValue(d->outputEdit->verticalScrollBar()->maximum());
     // QPlainTextEdit destroys the first calls value in case of multiline
     // text, so make sure that the scroll bar actually gets the value set.
     // Is a noop if the first call succeeded.
-    verticalScrollBar()->setValue(verticalScrollBar()->maximum());
+    d->outputEdit->verticalScrollBar()->setValue(d->outputEdit->verticalScrollBar()->maximum());
 }
 
 void OutputPane::appendCustomText(const QString &textIn, AppendMode mode, const QTextCharFormat &format)
 {
-    if (d->maxCharCount > 0 && document()->characterCount() >= d->maxCharCount) {
+    if (d->maxCharCount > 0 && d->outputEdit->document()->characterCount() >= d->maxCharCount) {
         qDebug() << "Maximum limit exceeded : " << d->maxCharCount;
         return;
     }
@@ -122,7 +134,7 @@ void OutputPane::appendCustomText(const QString &textIn, AppendMode mode, const 
     auto text = mode == OverWrite ? textIn.trimmed() : normalizeNewlines(doNewlineEnforcement(textIn));
     d->cursor.insertText(text, format);
 
-    if (d->maxCharCount > 0 && document()->characterCount() >= d->maxCharCount) {
+    if (d->maxCharCount > 0 && d->outputEdit->document()->characterCount() >= d->maxCharCount) {
         QTextCharFormat tmp;
         tmp.setFontWeight(QFont::Bold);
         d->cursor.insertText(doNewlineEnforcement(tr("Additional output omitted") + QLatin1Char('\n')), tmp);
@@ -159,13 +171,23 @@ void OutputPane::appendText(const QString &text, OutputFormat format, AppendMode
     appendCustomText(text, mode, textFormat);
 }
 
+QTextDocument *OutputPane::document() const
+{
+    return d->outputEdit->document();
+}
+
+QPlainTextEdit *OutputPane::edit() const
+{
+    return d->outputEdit;
+}
+
 OutputPane *OutputPane::instance()
 {
     static OutputPane *ins = new OutputPane();
     return ins;
 }
 
-void OutputPane::contextMenuEvent(QContextMenuEvent * event)
+void OutputPane::contextMenuEvent(QContextMenuEvent *event)
 {
     if (!d->menu)
         return;
@@ -174,16 +196,16 @@ void OutputPane::contextMenuEvent(QContextMenuEvent * event)
     d->menu->show();
 }
 
-QList<QAction*> OutputPane::actionFactory()
+QList<QAction *> OutputPane::actionFactory()
 {
-    QList<QAction*> list;
+    QList<QAction *> list;
 
     {
         auto action = new QAction(this);
         action->setText(tr("Copy"));
-        connect(action, &QAction::triggered, [this](){
-            if (!document()->toPlainText().isEmpty())
-                copy();
+        connect(action, &QAction::triggered, [this]() {
+            if (!d->outputEdit->document()->toPlainText().isEmpty())
+                d->outputEdit->copy();
         });
         list.append(action);
     }
@@ -191,9 +213,9 @@ QList<QAction*> OutputPane::actionFactory()
     {
         auto action = new QAction(this);
         action->setText(tr("Clear"));
-        connect(action, &QAction::triggered, [this](){
-            if (!document()->toPlainText().isEmpty())
-                clear();
+        connect(action, &QAction::triggered, [this]() {
+            if (!d->outputEdit->document()->toPlainText().isEmpty())
+                d->outputEdit->clear();
         });
         list.append(action);
     }
@@ -201,9 +223,9 @@ QList<QAction*> OutputPane::actionFactory()
     {
         auto action = new QAction(this);
         action->setText(tr("Select All"));
-        connect(action, &QAction::triggered, [this](){
-            if (!document()->toPlainText().isEmpty())
-                selectAll();
+        connect(action, &QAction::triggered, [this]() {
+            if (!d->outputEdit->document()->toPlainText().isEmpty())
+                d->outputEdit->selectAll();
         });
         list.append(action);
     }
