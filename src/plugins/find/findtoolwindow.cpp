51bab6512ce56b610cf732d5a4b32d0d0613a276
fuyou <fuyou@uniontech.com>
2024-04-22 15:00:20 +0800

    fix: [buildercore][codeeditor][find][valgrind] UI modified
    Log: [buildercore][codeeditor][find][valgrind] UI modified
    
    Bug: https://pms.uniontech.com/bug-view-240473.html

--- src/plugins/find/findtoolwindow.cpp
+++ src/plugins/find/findtoolwindow.cpp
@@ -86,15 +86,17 @@ FindToolWindow::~FindToolWindow()
 
 void FindToolWindow::setupUi()
 {
+    DFrame *mainPaneFrame = new DFrame(this);
     d->stackedWidget = new DStackedWidget();
-    QVBoxLayout *vLayout = new QVBoxLayout();
-    vLayout->setContentsMargins(0, 0, 0, 0);
+    QHBoxLayout *hLayout = new QHBoxLayout();
+    hLayout->setContentsMargins(0, 0, 0, 0);
 
-    DScrollArea *scrollArea = new DScrollArea();
+    DScrollArea *scrollArea = new DScrollArea(mainPaneFrame);
     scrollArea->setWidgetResizable(true);
     scrollArea->setWidget(d->stackedWidget);
     scrollArea->setLineWidth(0);
-    vLayout->addWidget(scrollArea);
+    mainPaneFrame->setLineWidth(0);
+    hLayout->addWidget(mainPaneFrame);
 
     QWidget *searchParamWidget = new QWidget();
     QWidget *searchResultWidget = new QWidget();
@@ -106,12 +108,11 @@ void FindToolWindow::setupUi()
     d->stackedWidget->addWidget(searchResultWidget);
     d->stackedWidget->setCurrentIndex(0);
 
-    setLayout(vLayout);
+    setLayout(hLayout);
 }
 
 void FindToolWindow::addSearchParamWidget(QWidget *parentWidget)
 {
-    parentWidget->setFixedSize(458, 220);
 
     QFormLayout *formLayout = new QFormLayout();
     parentWidget->setLayout(formLayout);
