cbc4ba832ce599b6ec10836f680aba8aa67a58b1
liuzhangjian <liuzhangjian@uniontech.com>
2024-04-22 16:55:43 +0800

    fix: [crash] The app crashed while saving the CmakeList file
    Application crashes due to multithreaded resource contention
    
    Log: fix bug
    Bug: https://pms.uniontech.com/bug-view-245731.html

--- src/plugins/symbol/transceiver/symbolreceiver.cpp
+++ src/plugins/symbol/transceiver/symbolreceiver.cpp
@@ -17,7 +17,7 @@ SymbolReceiver::SymbolReceiver(QObject *parent)
     : dpf::EventHandler (parent)
     , dpf::AutoEventHandlerRegister<SymbolReceiver> ()
 {
-
+    qRegisterMetaType<SymbolParseArgs>("SymbolParseArgs");
 }
 
 dpf::EventHandler::Type SymbolReceiver::type()
@@ -40,7 +40,10 @@ void SymbolReceiver::eventProcess(const dpf::Event &event)
         QString language = info.language();
         QString storage = FO::checkCreateDir(FO::checkCreateDir(workspace, ".unioncode"), "symbol");
         if (!language.isEmpty() && !storage.isEmpty()) {
-            SymbolKeeper::instance()->doParse({workspace, language, storage});
+            SymbolParseArgs args { workspace, language, storage };
+            QMetaObject::invokeMethod(SymbolKeeper::instance(), "doParse",
+                                      Qt::QueuedConnection,
+                                      Q_ARG(SymbolParseArgs, args));
         }
         SymbolKeeper::instance()->treeView()->setRootPath(storage);
     } else if (event.data() == symbol.parse.name) { // "workspace", "language", "storage"
