753a341fde51d62dcbb639b279296f52f7689383
ZHANGTINGAN <zhangtingan@uniontech.com>
2024-04-22 17:04:09 +0800

    feat: [cmake] add configuration of cmake
    add configuration of cmake

---
+++ src/plugins/cxx/cmake/project/properties/cmakeCfgWidget/cmakepropertypage.h
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef CMAKEPROPERTYPAGE_H
+#define CMAKEPROPERTYPAGE_H
+
+#include "cmakeitemmodel.h"
+
+#include <DWidget>
+
+#include <QString>
+
+class CMakePropertyPagePrivate;
+class CMakePropertyPage : public DTK_WIDGET_NAMESPACE::DWidget
+{
+    Q_OBJECT
+public:
+    explicit CMakePropertyPage(QWidget *parent = nullptr);
+    ~CMakePropertyPage();
+
+    void saveConfigToCacheFile(const QString &cacheFile);
+    void getItemsFromCacheFile(const QString &cacheFile);
+
+    void setupUi();
+
+signals:
+    void cacheFileUpdated();
+
+private:
+    CMakePropertyPagePrivate *d;
+};
+
+#endif // CMAKEPROPERTYPAGE_H
