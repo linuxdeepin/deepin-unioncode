56fcaa01b5d74a003487bf793d2f38b2655fbf7e
liuzhangjian <liuzhangjian@uniontech.com>
2024-04-28 14:45:29 +0800

    feat: [git] Git log of current file
    Add git log for current displyed file
    
    Log: git log

--- src/plugins/git/client/gitclient.cpp
+++ src/plugins/git/client/gitclient.cpp
@@ -6,19 +6,28 @@
 #include "constants.h"
 #include "utils/gitcommand.h"
 #include "gui/instantblamewidget.h"
+#include "gui/gittabwidget.h"
+
+#include "services/window/windowservice.h"
 
 #include <QDir>
 #include <QFile>
 #include <QTimer>
 #include <QDebug>
 
+using namespace dpfservice;
+
 class GitClientPrivate : public QObject
 {
 public:
     QString findRepository(const QString &filePath);
+    QString normalLogArguments();
+
     void instantBlame();
+    void logFile(const QString &workspace, const QString &filePath);
 
 public:
+    QString lastCentral;
     QStringList gitRepositoryCache;
     QTimer *cursorPositionChangedTimer { nullptr };
     QString currentWorkspace;
@@ -26,6 +35,7 @@ public:
     int currentLine { 0 };
 
     InstantBlameWidget *ibWidget { nullptr };
+    GitTabWidget *gitTabWidget { nullptr };
 };
 
 QString GitClientPrivate::findRepository(const QString &filePath)
@@ -39,10 +49,28 @@ QString GitClientPrivate::findRepository(const QString &filePath)
 
     QDir dir(filePath);
     auto dirList = dir.entryList(QDir::AllDirs | QDir::Hidden | QDir::NoDotAndDotDot);
-    if (dirList.isEmpty() || !dirList.contains(".git"))
+    if (dirList.isEmpty() || !dirList.contains(GitDirectory))
         return findRepository(info.absolutePath());
 
     return filePath;
+}
+
+QString GitClientPrivate::normalLogArguments()
+{
+    const QString authorName("#007af4");
+    const QString commitDate("#006600");
+    const QString commitHash("#ff0000");
+    const QString decoration("#ff00ff");
+
+    const QString logArgs = QStringLiteral(
+                                    "--pretty=format:"
+                                    "commit %C(%1)%H%Creset %C(%2)%d%Creset%n"
+                                    "Author: %C(%3)%an <%ae>%Creset%n"
+                                    "Date:   %C(%4)%cD %Creset%n%n"
+                                    "%w(0,4,4)%s%Creset%n%n%b")
+                                    .arg(commitHash, decoration, authorName, commitDate);
+
+    return logArgs;
 }
 
 void GitClientPrivate::instantBlame()
@@ -62,6 +90,31 @@ void GitClientPrivate::instantBlame()
     arguments.append({ "-L", lineString, "--", currentFilePath });
     cmd->addJob(GitBinaryPath, arguments);
 
+    cmd->start();
+}
+
+void GitClientPrivate::logFile(const QString &workspace, const QString &filePath)
+{
+    int index = gitTabWidget->addWidget(GitTabWidget::GitLog, filePath);
+    GitCommand *cmd = new GitCommand(workspace);
+    connect(cmd, &GitCommand::finished, this, [=](int code) {
+        if (code == 0) {
+            gitTabWidget->setInfo(index, cmd->cleanedStdOut());
+        } else {
+            gitTabWidget->setInfo(index, tr("Failed to retrieve data."));
+            qWarning() << cmd->cleanedStdErr();
+        }
+
+        cmd->deleteLater();
+    });
+
+    QStringList arguments = { "log", DecorateOption,
+                              "-n", QString::number(LogMaxCount),
+                              "--patience", "--ignore-space-change",
+                              ColorOption, "--follow",
+                              normalLogArguments(), "--", filePath };
+
+    cmd->addJob(GitBinaryPath, arguments);
     cmd->start();
 }
 
@@ -85,6 +138,14 @@ GitClient *GitClient::instance()
 void GitClient::init()
 {
     d->ibWidget = new InstantBlameWidget;
+    d->gitTabWidget = new GitTabWidget;
+
+    connect(d->gitTabWidget, &GitTabWidget::closeRequested, this, &GitClient::switchLastCentralWidget);
+}
+
+void GitClient::setLastCentralWidget(const QString &name)
+{
+    d->lastCentral = name;
 }
 
 QString GitClient::gitBinaryPath() const
@@ -97,28 +158,53 @@ bool GitClient::gitBinaryValid()
     return QFile::exists(GitBinaryPath);
 }
 
-bool GitClient::setupInstantBlame(const QString &filePath)
+bool GitClient::checkRepositoryExist(const QString &filePath, QString *repository)
 {
-    if (filePath.isEmpty())
-        return false;
-
     for (const auto &cache : d->gitRepositoryCache) {
-        if (filePath.startsWith(cache))
+        if (filePath.startsWith(cache)) {
+            if (repository)
+                *repository = cache;
             return true;
+        }
     }
 
-    auto repository = d->findRepository(filePath);
-    if (!repository.isEmpty()) {
-        d->gitRepositoryCache << repository;
+    auto ret = d->findRepository(filePath);
+    if (!ret.isEmpty()) {
+        d->gitRepositoryCache << ret;
+        if (repository)
+            *repository = ret;
         return true;
     }
 
     return false;
 }
 
+bool GitClient::setupInstantBlame(const QString &filePath)
+{
+    if (filePath.isEmpty())
+        return false;
+
+    return checkRepositoryExist(filePath);
+}
+
+bool GitClient::logFile(const QString &filePath)
+{
+    QString repository;
+    if (!checkRepositoryExist(filePath, &repository))
+        return false;
+
+    d->logFile(repository, filePath);
+    return true;
+}
+
 QWidget *GitClient::instantBlameWidget() const
 {
     return d->ibWidget;
+}
+
+QWidget *GitClient::gitTabWidget() const
+{
+    return d->gitTabWidget;
 }
 
 void GitClient::instantBlame(const QString &workspace, const QString &filePath, int line)
@@ -135,4 +221,18 @@ void GitClient::instantBlame(const QString &workspace, const QString &filePath, 
     d->currentFilePath = filePath;
     d->currentLine = line;
     d->cursorPositionChangedTimer->start();
+}
+
+void GitClient::switchLastCentralWidget()
+{
+    if (d->lastCentral.isEmpty())
+        return;
+
+    auto &ctx = dpfInstance.serviceContext();
+    WindowService *windowService = ctx.service<WindowService>(WindowService::name());
+    if (!windowService)
+        return;
+
+    windowService->showWidgetAtPosition(d->lastCentral, Position::Central, true);
+    d->lastCentral.clear();
 }
---
+++ src/plugins/git/utils/gitmenumanager.cpp
@@ -0,0 +1,103 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "gitmenumanager.h"
+#include "constants.h"
+#include "client/gitclient.h"
+
+#include "base/abstractaction.h"
+#include "services/window/windowservice.h"
+
+#include <QMenu>
+
+using namespace dpfservice;
+
+GitMenuManager::GitMenuManager(QObject *parent)
+    : QObject(parent)
+{
+}
+
+GitMenuManager *GitMenuManager::instance()
+{
+    static GitMenuManager ins;
+    return &ins;
+}
+
+void GitMenuManager::initialize(dpfservice::WindowService *service)
+{
+    if (!service)
+        return;
+
+    winSer = service;
+    auto initAction = [&](QAction *action, const QString &id = QString(),
+                          const QString &description = QString(),
+                          const QKeySequence &key = QKeySequence()) -> AbstractAction * {
+        auto actionImpl = new AbstractAction(action, this);
+        if (!key.isEmpty())
+            actionImpl->setShortCutInfo(id, description, key);
+        return actionImpl;
+    };
+
+    QAction *gitAct = new QAction("&Git", this);
+    auto gitActImpl = initAction(gitAct);
+    service->addAction(MWM_TOOLS, gitActImpl);
+
+    createGitSubMenu();
+    gitAct->setMenu(&gitSubMenu);
+}
+
+void GitMenuManager::setCurrentProject(const QString &project)
+{
+    // TODO: act
+}
+
+void GitMenuManager::setCurrentFile(const QString &file)
+{
+    if (file.isEmpty() || !GitClient::instance()->checkRepositoryExist(file)) {
+        curFileAct->setEnabled(false);
+        return;
+    }
+
+    curFileAct->setEnabled(true);
+    QFileInfo info(file);
+
+    fileLogAct->setProperty(GitFilePath, file);
+    fileLogAct->setText(tr("Log of \"%1\"").arg(info.fileName()));
+}
+
+void GitMenuManager::createGitSubMenu()
+{
+    curFileAct = gitSubMenu.addAction(tr("Current File"));
+    curFileAct->setEnabled(false);
+    curFileAct->setMenu(&fileSubMenu);
+    createFileSubMenu();
+
+    curProjectAct = gitSubMenu.addAction(tr("Current Project"));
+    curProjectAct->setEnabled(false);
+    curProjectAct->setMenu(&projectSubMenu);
+    createProjectSubMenu();
+}
+
+void GitMenuManager::createFileSubMenu()
+{
+    fileLogAct = new QAction(this);
+    connect(fileLogAct, &QAction::triggered, this, [this] {
+        const auto &filePath = fileLogAct->property(GitFilePath).toString();
+        if (GitClient::instance()->logFile(filePath)) {
+            auto dockName = winSer->getCurrentDockName(Position::Central);
+            if (dockName == GitWindow)
+                return;
+
+            GitClient::instance()->setLastCentralWidget(dockName);
+            winSer->showWidgetAtPosition(GitWindow, Position::Central, true);
+        }
+    });
+
+    fileSubMenu.addAction(fileLogAct);
+}
+
+void GitMenuManager::createProjectSubMenu()
+{
+    // TODO: create
+}
