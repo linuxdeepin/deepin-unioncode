78e25897763868160e2c7bb9fd00a2e73e2168d6
Zhang TingAn <zhangtingan@uniontech.com>
2024-05-07 15:46:05 +0800

    feat: [debugger] remote debug
    support incomplete function of remote debug

--- src/plugins/debugger/dap/debugsession.cpp
+++ src/plugins/debugger/dap/debugsession.cpp
@@ -132,6 +132,21 @@ bool DebugSession::attach(dap::AttachRequest &config)
     return ret.valid();
 }
 
+void DebugSession::setRemote(bool on)
+{
+    isRemote = on;
+}
+
+void DebugSession::setLocalProjectPath(const QString &path)
+{
+    localProjectPath = path;
+}
+
+void DebugSession::setRemoteProjectPath(const QString &path)
+{
+    remoteProjectPath = path;
+}
+
 void DebugSession::restart()
 {
     if (!raw)
@@ -488,7 +503,7 @@ void DebugSession::sendBreakpoints(const QString &sourcePath, dap::array<IBreakp
     dap::array<SourceBreakpoint> breakpoints;
     for (auto it : breakpointsToSend) {
         dap::Source source;
-        source.path = it.uri.toString().toStdString();
+        source.path = isRemote ? transformLocalPath(it.uri.toString()).toStdString() : it.uri.toString().toStdString();
         source.name = undefined;
         request.source = source;
         SourceBreakpoint bt;
@@ -863,6 +878,19 @@ void DebugSession::disassemble(const dap::string &address)
     DisassembleRequest request;
     request.memoryReference = address;
     raw->disassemble(request);
+}
+
+QString DebugSession::transformLocalPath(const QString &localPath)
+{
+    if (!isRemote || remoteProjectPath.isEmpty())
+        return localPath;
+
+    QString ret = localPath;
+    if (localPath.startsWith(localProjectPath)) {
+        ret.replace(0, localProjectPath.size(), remoteProjectPath);
+    }
+
+    return ret;
 }
 
 }   // endnamespace
