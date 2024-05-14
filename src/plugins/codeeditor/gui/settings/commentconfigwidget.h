ba59a0d0b2c4e0972cb3c057631fde11249700f3
fuyou <fuyou@uniontech.com>
2024-05-14 13:45:45 +0800

    feat: [codeeditor] Add Comment
    Log: Add comment by shortcut: ctrl + shift + /

---
+++ src/plugins/codeeditor/gui/settings/commentconfigwidget.h
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef COMMENTCONFIGWIDGET_H
+#define COMMENTCONFIGWIDGET_H
+
+#include "common/widget/pagewidget.h"
+
+class CommentConfigWidgetPrivate;
+class CommentConfigWidget : public PageWidget
+{
+    Q_OBJECT
+public:
+    explicit CommentConfigWidget(QWidget *parent = nullptr);
+    ~CommentConfigWidget() override;
+
+    void initUI();
+    void initConnections();
+
+    void setUserConfig(const QMap<QString, QVariant> &map) override;
+    void getUserConfig(QMap<QString, QVariant> &map) override;
+
+public slots:
+
+private:
+    CommentConfigWidgetPrivate *const d;
+};
+
+#endif // COMMENTCONFIGWIDGET_H
