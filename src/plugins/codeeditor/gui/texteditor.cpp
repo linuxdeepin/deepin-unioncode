717567502f48c3f65ef00ef8a48c1c36648af16c
fuyou <fuyou@uniontech.com>
2024-05-17 09:27:41 +0800

    fix: [codeeditor] comment logic && ui modified
    Log: add single-line comment without selected, ui modified

--- src/plugins/codeeditor/gui/texteditor.cpp
+++ src/plugins/codeeditor/gui/texteditor.cpp
@@ -425,6 +425,7 @@ bool TextEditor::hasUncommentedLines(const int& lineFrom, const int& lineTo, con
             return false;
         return true;
     }
+    setSelection(lineFrom, indexFrom, lineTo, indexTo);
     QStringList lines = selectedText().split(QRegExp("\\r\\n|\\n|\\r")); 
     for (const QString &line : lines) {
         if (line.trimmed().isEmpty())
