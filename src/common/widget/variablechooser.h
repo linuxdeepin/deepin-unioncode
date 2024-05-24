28cc297fa65b03f1e20b5e5fea264e0e826b8a0e
liuzhangjian <liuzhangjian@uniontech.com>
2024-05-24 14:09:06 +0800

    feat: [tool] Add spell check tool
    1.Add macro support
    2.Add spell check tool

---
+++ src/common/widget/variablechooser.h
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef VARIABLECHOOSER_H
+#define VARIABLECHOOSER_H
+
+#include "common/util/macroexpander.h"
+
+#include <DDialog>
+
+#include <functional>
+
+using MacroExpanderProvider = std::function<MacroExpander *()>;
+
+class QLineEdit;
+class VariableChooserPrivate;
+class VariableChooser : public DTK_WIDGET_NAMESPACE::DDialog
+{
+    Q_OBJECT
+public:
+    explicit VariableChooser(QWidget *parent = nullptr);
+    ~VariableChooser();
+
+    void addMacroExpander(MacroExpander *expander);
+    void addSupportedEdit(QLineEdit *edit, const QByteArray &ownName = QByteArray());
+
+protected:
+    bool eventFilter(QObject *obj, QEvent *e) override;
+
+private:
+    VariableChooserPrivate *const d;
+};
+
+#endif   // VARIABLECHOOSER_H
