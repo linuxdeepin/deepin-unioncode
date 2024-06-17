fa82e891eb123dc9a96828b5893e64e4e7bbb29e
liuzhangjian <liuzhangjian@uniontech.com>
2024-06-17 14:03:57 +0800

    fix: [symbol] Fixed an issue where the symbol page had no content
    as title
    
    Log: fix issue

---
+++ src/plugins/symbol/util/util.h
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <QString>
+
+class Util
+{
+public:
+    static bool checkPackageValid(const QString &package);
+    static void installPackage(const QString &package);
+};
+
+#endif // UTIL_H
