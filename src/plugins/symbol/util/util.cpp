3776f3dd9522fdc5e594d15731448e2fbbaefcab
liuzhangjian <liuzhangjian@uniontech.com>
2024-06-17 14:03:57 +0800

    fix: [symbol] Fixed an issue where the symbol page had no content
    as title
    
    Log: fix issue

---
+++ src/plugins/symbol/util/util.cpp
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "util.h"
+
+#include <QProcess>
+
+bool Util::checkPackageValid(const QString &package)
+{
+    QProcess process;
+    QString cmd("pip3 show %1");
+    process.start(cmd.arg(package));
+    process.waitForFinished();
+
+    QString output = process.readAllStandardOutput();
+    return !output.isEmpty();
+}
+
+void Util::installPackage(const QString &package)
+{
+    QProcess process;
+    QString cmd("pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple %1");
+    process.start(cmd.arg(package));
+    process.waitForFinished();
+}
