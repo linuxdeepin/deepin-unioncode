41eddaca30f743c5a1b55ef51ec34451372d2dae
Zhang TingAn <zhangtingan@uniontech.com>
2024-06-21 16:42:13 +0800

    feat: [linglong] linglong
    1.support generate linglong.yaml
    2.support create a linglong project, use ll-builder run / build to manipulate it
    3.display of linglong package
    
    log: new plugin

---
+++ src/plugins/linglong/gui/mainframe.h
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef MAINFRAME_H
+#define MAINFRAME_H
+
+#include "common/widget/appoutputpane.h"
+
+#include <DFrame>
+
+class MainFramePrivate;
+class MainFrame : public DTK_WIDGET_NAMESPACE::DFrame
+{
+public:
+    MainFrame(QWidget *parent = nullptr);
+    ~MainFrame();
+
+private slots:
+    void runApp(const QString &appId);
+    void installApp(const QString &appId);
+    void uninstallApp(const QString &appId);
+
+private:
+    MainFramePrivate *d { nullptr };
+
+    void initUi();
+    void initConnection();
+    void initTable();
+    void updateInstalled();
+    void updateRepository(const QString &text);
+    void updateRunning();
+
+    void output(const QString &text, OutputPane::OutputFormat format = OutputPane::NormalMessage);
+};
+
+#endif   // MAINFRAME_H
