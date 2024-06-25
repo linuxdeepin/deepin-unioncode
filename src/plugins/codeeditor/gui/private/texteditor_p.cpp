c817068eef129646210e745593878405086a195b
fuyou <fuyou@uniontech.com>
2024-06-25 11:47:00 +0800

    chore: [translation] updated translation
    Log: updated translation
    
    Bug: https://pms.uniontech.com/bug-view-261509.html, https://pms.uniontech.com/bug-view-259621.html

--- src/plugins/codeeditor/gui/private/texteditor_p.cpp
+++ src/plugins/codeeditor/gui/private/texteditor_p.cpp
@@ -328,7 +328,7 @@ void TextEditorPrivate::showMarginMenu()
             menu.addAction(tr("Enable Breakpoint"), q, [this, line] { q->setBreakpointEnabled(line, true); });
         menu.addAction(tr("Add Condition"), q, [this, line] { q->setBreakpointCondition(line); });
     } else {
-        static QString text("Add a breakpoint on line %1");
+        static QString text(tr("Add a breakpoint on line %1"));
         menu.addAction(text.arg(line + 1), q, [this, line] { q->addBreakpoint(line); });
     }
 
