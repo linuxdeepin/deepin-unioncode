800b62e6bc99daa1e08d206a2e293305237d59dd
liuzhangjian <liuzhangjian@uniontech.com>
2024-07-02 10:39:54 +0800

    chore: Update license
    as title
    
    Log: update license

--- src/common/widget/elidedlabel.h
+++ src/common/widget/elidedlabel.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
 //
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 
 #ifndef ELIDEDLABEL_H
 #define ELIDEDLABEL_H
--- src/common/widget/pagewidget.h
+++ src/common/widget/pagewidget.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
 //
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 
 #ifndef PAGEWIDGET_H
 #define PAGEWIDGET_H
