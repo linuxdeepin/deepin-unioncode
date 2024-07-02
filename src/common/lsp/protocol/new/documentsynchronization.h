06fde2dadbc47b8dbd563d6313fdf5d814c8532a
liuzhangjian <liuzhangjian@uniontech.com>
2024-07-02 10:39:54 +0800

    chore: Update license
    as title
    
    Log: update license

--- src/common/lsp/protocol/new/documentsynchronization.h
+++ src/common/lsp/protocol/new/documentsynchronization.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
 //
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 
 #ifndef DOCUMENTSYNCHRONIZATION_H
 #define DOCUMENTSYNCHRONIZATION_H
--- src/common/unilog.h
+++ src/common/unilog.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2022 - 2023 UnionTech Software Technology Co., Ltd.
 //
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 #ifndef UNILOG_H
 #define UNILOG_H
 
