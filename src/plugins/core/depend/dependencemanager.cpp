80c8786290aa5dab930c02d871496a64ed2f7539
liuzhangjian <liuzhangjian@uniontech.com>
2024-08-05 10:05:36 +0800

    feat: [Dependence] Added the ability to view details
    as title
    
    Log: view installtion details

--- src/plugins/core/depend/dependencemanager.cpp
+++ src/plugins/core/depend/dependencemanager.cpp
@@ -4,24 +4,38 @@
 
 #include "dependencemanager.h"
 #include "notify/notificationmanager.h"
+#include "uicontroller/controller.h"
 
+#include "base/baseitemdelegate.h"
+
+#include <DDialog>
+#include <DLabel>
+
+#include <QListView>
+#include <QStringListModel>
 #include <QMap>
 #include <QTimer>
 #include <QDebug>
 #include <QMutexLocker>
 #include <QtConcurrent>
+#include <QVBoxLayout>
 
 #include <functional>
 
 const char CancelActionID[] { "cancel" };
+const char ViewDetails[] { "view_details" };
 const char InstallActionID[] { "install_default" };
+
+DWIDGET_USE_NAMESPACE
 
 class DependenceManagerPrivate : public QObject
 {
 public:
     void init();
     void notify();
-    void installHander(const QString &actId, const InstallInfo &info);
+    void handleActionInvoke(const QString &actId, const InstallInfo &info);
+    void install(const InstallInfo &info);
+    void showDetails(const InstallInfo &info);
     void checkInstalled(const QString &plugin, const QString &name, const QStringList &packageList);
 
 public:
@@ -47,27 +61,65 @@ void DependenceManagerPrivate::notify()
         notifyTimer.start();
 
     QStringList actionList { CancelActionID, DependenceManager::tr("Cancel"),
+                             ViewDetails, DependenceManager::tr("View Details"),
                              InstallActionID, DependenceManager::tr("Install") };
 
     QMutexLocker lk(&mutex);
     while (!infoList.isEmpty()) {
         const auto &info = infoList.takeFirst();
-        auto cb = std::bind(&DependenceManagerPrivate::installHander, this, _1, info);
+        auto cb = std::bind(&DependenceManagerPrivate::handleActionInvoke, this, _1, info);
         QString msg = DependenceManager::tr("Request to install some dependency packages. "
                                             "Do you want to install them?");
         NotificationManager::instance()->notify(0, info.plugin, msg, actionList, cb);
     }
 }
 
-void DependenceManagerPrivate::installHander(const QString &actId, const InstallInfo &info)
+void DependenceManagerPrivate::handleActionInvoke(const QString &actId, const InstallInfo &info)
 {
-    if (actId != InstallActionID)
-        return;
+    if (actId == InstallActionID)
+        install(info);
+    else if (actId == ViewDetails)
+        showDetails(info);
+}
 
+void DependenceManagerPrivate::install(const InstallInfo &info)
+{
     if (!installerMap.contains(info.installer))
         return;
 
     installerMap[info.installer]->install(info);
+}
+
+void DependenceManagerPrivate::showDetails(const InstallInfo &info)
+{
+    DDialog dlg(Controller::instance()->mainWindow());
+    dlg.setIcon(QIcon::fromTheme("ide"));
+    dlg.setWindowTitle(DependenceManager::tr("Installation Details"));
+    dlg.addButton(DependenceManager::tr("Cancel", "button"));
+    dlg.addButton(DependenceManager::tr("Install", "button"), true, DDialog::ButtonRecommend);
+
+    QWidget *widget = new QWidget(&dlg);
+    QVBoxLayout *layout = new QVBoxLayout(widget);
+    layout->setContentsMargins(0, 0, 0, 0);
+
+    DLabel *label = new DLabel(DependenceManager::tr("Installation package list:"));
+    label->setWordWrap(true);
+
+    QListView *view = new QListView(&dlg);
+    QStringListModel *model = new QStringListModel(view);
+    model->setStringList(info.packageList);
+    view->setModel(model);
+    view->setEditTriggers(QListView::NoEditTriggers);
+    view->setFrameShape(QFrame::NoFrame);
+    view->setItemDelegate(new BaseItemDelegate(view));
+    view->setSizeAdjustPolicy(QListView::AdjustToContents);
+
+    layout->addWidget(label);
+    layout->addWidget(view, 1);
+    dlg.addContent(widget);
+    int code = dlg.exec();
+    if (code == 1)
+        install(info);
 }
 
 void DependenceManagerPrivate::checkInstalled(const QString &plugin, const QString &name, const QStringList &packageList)
