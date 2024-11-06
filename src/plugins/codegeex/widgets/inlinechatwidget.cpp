68d238a8fb291197a7557a1f1b26f25974e61503
Liu Zhangjian <liuzhangjian@uniontech.com>
2024-11-06 15:59:16 +0800

    fix: [inline chat] The keypad Enter key cannot trigger a submit request
    as title
    
    Log: fix bug
    Bug: https://pms.uniontech.com/bug-view-278015.html

--- src/plugins/codegeex/widgets/inlinechatwidget.cpp
+++ src/plugins/codegeex/widgets/inlinechatwidget.cpp
@@ -851,6 +851,7 @@ bool InlineChatWidget::eventFilter(QObject *obj, QEvent *e)
                 break;
             }
         }
+        case Qt::KeypadModifier:
         case Qt::NoModifier: {
             switch (ke->key()) {
             case Qt::Key_Enter:
