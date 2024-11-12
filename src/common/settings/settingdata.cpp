d459b2cd3068b0b76140d92ea41e1fe56bfe6471
Liu Zhangjian <liuzhangjian@uniontech.com>
2024-11-12 14:09:32 +0800

    feat: [settings] Add a general configuration module
    as title
    
    Log: Add a general configuration module

---
+++ src/common/settings/settingdata.cpp
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "settingdata.h"
+
+QVariant SettingData::value(const QString &group, const QString &key, const QVariant &dv) const
+{
+    return values.value(group).value(key, dv);
+}
+
+void SettingData::setValue(const QString &group, const QString &key, const QVariant &value)
+{
+    if (!values.contains(group)) {
+        values.insert(group, { { key, value } });
+        return;
+    }
+
+    values[group][key] = value;
+}
---
+++ src/common/settings/settings.h
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: 2024 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include <QObject>
+#include <QVariant>
+
+class SettingsPrivate;
+class Settings : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Settings(const QString &defaultFile, const QString &settingFile, QObject *parent = nullptr);
+    ~Settings();
+
+    QStringList groupList() const;
+    QStringList keyList(const QString &group) const;
+    bool contains(const QString &group, const QString &key) const;
+    QVariant value(const QString &group, const QString &key, const QVariant &defaultValue = QVariant());
+    QVariant defaultValue(const QString &group, const QString &key, const QVariant &defaultValue = QVariant());
+    void setValue(const QString &group, const QString &key, const QVariant &value, bool notify = false);
+    void removeGroup(const QString &group);
+    void remove(const QString &group, const QString &key);
+    void clear();
+    void reload();
+    bool sync();
+    bool isAutoSync();
+    void setSyncExcludeGroup(const QString &group, bool sync = false);
+
+public Q_SLOTS:
+    void setAutoSync(bool autoSync);
+
+Q_SIGNALS:
+    void valueChanged(const QString &group, const QString &key, const QVariant &value);
+
+private:
+    SettingsPrivate *const d;
+};
+
+#endif   // SETTINGS_H
