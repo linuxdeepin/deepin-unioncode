7b95fdf8f743e14af14d497899248ec9fc723198
Lu Zhen <luzhen@uniontech.com>
2025-01-13 14:01:42 +0800

    chore: simplify jsonrpc
    Log:
    Change-Id: I097725299845c5cce0e37b8c7b4f5ccb17f5716b

--- 3rdparty/unioncode-jsonrpccpp/libjson-rpc-cpp/docker/deps-debian.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -evu
-apt-get update && apt-get update && apt-get install -y wget build-essential cmake libjsoncpp-dev libargtable2-dev libcurl4-openssl-dev libmicrohttpd-dev git libhiredis-dev redis-server lcov curl
