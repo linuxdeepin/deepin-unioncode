5f32126f950dd992344541d98be34a45b3a95a4e
Lu Zhen <luzhen@uniontech.com>
2025-01-21 09:58:22 +0800

    fix: build faild
    Log:
    Change-Id: Ibe88a57b4f761a3e2d7fa83605c78e7cf6d34399

--- src/plugins/smartut/manager/uttaskpool.h
+++ src/plugins/smartut/manager/uttaskpool.h
@@ -44,7 +44,7 @@ Q_SIGNALS:
     void stoped(NodeItem *item);
 
 private:
-    bool createModels(const QString &model);
+    void createModels(const QString &model);
 
     QQueue<Task> taskQueue;
     QMap<QString, QList<AbstractLLM *>> idleModels;
