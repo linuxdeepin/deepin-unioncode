607d8a2b718020da340869e52a580d2ee3f2a053
Lu Zhen <luzhen@uniontech.com>
2025-05-26 16:38:53 +0800

    chore: update spdx info
    Log:
    Change-Id: I95b31b5ac10f9ea3217019cca628a45b0797f4ac

--- src/scripts/container-up.sh
+++ src/scripts/container-up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
+# SPDX-FileCopyrightText: 2023 - 2025 UnionTech Software Technology Co., Ltd.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 PLATFORM="$HOME/.config/unioncode/configures/platform"
--- src/scripts/symbol-parser/__init__.py
+++ src/scripts/symbol-parser/__init__.py
@@ -1,3 +1,3 @@
-# SPDX-FileCopyrightText: 2022 - 2023 UnionTech Software Technology Co., Ltd.
+# SPDX-FileCopyrightText: 2022 - 2025 UnionTech Software Technology Co., Ltd.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
